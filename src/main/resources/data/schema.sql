48f294fff5e94e15e2ab96e3616b365237f8b52c
Aziz <aziz.owidat@gmail.com>
2022-06-12 13:50:33 +0200

    update logging

--- src/main/resources/data/schema.sql
+++ src/main/resources/data/schema.sql
@@ -1,40 +1,31 @@
-create table if not exists category
+create table if not exists public.category
 (
-    id   integer not null
-        primary key,
-    name varchar(255)
+    id        integer not null,
+    name      varchar(255),
+    parnet_id integer,
+    primary key (id),
+    constraint fkjuhi5c41octuei0syqcc1akv9
+        foreign key (parnet_id) references public.category
 );
 
-create table if not exists parent_of_category
+create table if not exists public.person
 (
-    parent_id integer not null
-        constraint fkqnro3m6pqh3yilfiq13sjt3jn
-            references category,
-    child_id  integer not null
-        constraint fkrgjbc28o6k2bhvpvsaqs4mfpm
-            references category
+    id   integer not null,
+    name varchar(255),
+    primary key (id)
 );
 
-create table if not exists person
+create table if not exists public.product
 (
-    id   integer not null
-        primary key,
-    name varchar(255)
-);
-
-create table if not exists product
-(
-    id         varchar(255)     not null
-        primary key,
+    id         varchar(255) not null,
     image      text,
-    price      double precision not null,
-    rating     integer          not null,
-    sales_rank integer          not null,
-    status     varchar(255),
-    title      varchar(255)
+    rating     integer      not null,
+    sales_rank integer      not null,
+    title      varchar(255),
+    primary key (id)
 );
 
-create table if not exists book
+create table if not exists public.book
 (
     binding     varchar(255),
     edition     varchar(255),
@@ -44,69 +35,69 @@ create table if not exists book
     page        integer      not null,
     publication date,
     weight      integer      not null,
-    id          varchar(255) not null
-        primary key
-        constraint fk8cjf4cjanicu58p2l5t8d9xvu
-            references product
+    id          varchar(255) not null,
+    primary key (id),
+    constraint fk8cjf4cjanicu58p2l5t8d9xvu
+        foreign key (id) references public.product
 );
 
-create table if not exists author
+create table if not exists public.author
 (
-    book_id   varchar(255) not null
-        constraint fkqi5nll4mal57ohohlv5g0qlv2
-            references book,
-    person_id integer      not null
-        constraint fkd8cm5fy8qunfotg4xpdi1wad7
-            references person
+    book_id   varchar(255) not null,
+    person_id integer      not null,
+    constraint fkd8cm5fy8qunfotg4xpdi1wad7
+        foreign key (person_id) references public.person,
+    constraint fkqi5nll4mal57ohohlv5g0qlv2
+        foreign key (book_id) references public.book
 );
 
-create table if not exists book_publisher
+create table if not exists public.book_publisher
 (
-    book_id   varchar(255) not null
-        constraint fk8ywuvxfycghsfmxvu363jllpq
-            references book,
-    publisher varchar(255)
+    book_id   varchar(255) not null,
+    publisher varchar(255),
+    constraint fk8ywuvxfycghsfmxvu363jllpq
+        foreign key (book_id) references public.book
 );
 
-create table if not exists cd
+create table if not exists public.cd
 (
     binding varchar(255),
     date    date,
     disc_nr integer      not null,
     format  varchar(255),
-    id      varchar(255) not null
-        primary key
-        constraint fkg450nkyhi3a0t7kn2cosol0xf
-            references product
+    id      varchar(255) not null,
+    primary key (id),
+    constraint fkg450nkyhi3a0t7kn2cosol0xf
+        foreign key (id) references public.product
 );
 
-create table if not exists artist
+create table if not exists public.artist
 (
-    cd_id     varchar(255) not null
-        constraint fk3xd8j8p11jo7rrcphikx4i0u
-            references cd,
-    person_id integer      not null
-        constraint fk42u4bruy3o7qi1jlqvreu49ak
-            references person
+    cd_id     varchar(255) not null,
+    person_id integer      not null,
+    constraint fk42u4bruy3o7qi1jlqvreu49ak
+        foreign key (person_id) references public.person,
+    constraint fk3xd8j8p11jo7rrcphikx4i0u
+        foreign key (cd_id) references public.cd
 );
 
-create table if not exists cd_labels
+create table if not exists public.cd_labels
 (
-    cd_id  varchar(255) not null
-        constraint fkjqlfqvxce9dygyvl1dkh8xe24
-            references cd,
-    labels varchar(255)
+    cd_id  varchar(255) not null,
+    labels varchar(255),
+    constraint fkjqlfqvxce9dygyvl1dkh8xe24
+        foreign key (cd_id) references public.cd
 );
 
-create table if not exists cd_tracks
+create table if not exists public.cd_tracks
 (
-    cd_id  varchar(255) not null
-        constraint fk66a5xwpfjq2uu32hndlfsl71m
-            references cd,
-    tracks varchar(255)
+    cd_id  varchar(255) not null,
+    tracks varchar(255),
+    constraint fk66a5xwpfjq2uu32hndlfsl71m
+        foreign key (cd_id) references public.cd
 );
 
-create table if not exists dvd
+create table if not exists public.dvd
 (
     aspectratio     varchar(255),
     format          varchar(255),
@@ -114,123 +105,129 @@ create table if not exists dvd
     release_date    date,
     running_time    integer      not null,
     theater_release integer      not null,
-    id              varchar(255) not null
-        primary key
-        constraint fk8767xtav39bmxqpras2ivshb9
-            references product
+    id              varchar(255) not null,
+    primary key (id),
+    constraint fk8767xtav39bmxqpras2ivshb9
+        foreign key (id) references public.product
 );
 
-create table if not exists actor
+create table if not exists public.actor
 (
-    dvd_id    varchar(255) not null
-        constraint fk865vtt4l7wh5qwua14meyqvkc
-            references dvd,
-    person_id integer      not null
-        constraint fktk6djt6y6ni04hiton4o64s1f
-            references person,
-    primary key (dvd_id, person_id)
+    dvd_id    varchar(255) not null,
+    person_id integer      not null,
+    primary key (dvd_id, person_id),
+    constraint fktk6djt6y6ni04hiton4o64s1f
+        foreign key (person_id) references public.person,
+    constraint fk865vtt4l7wh5qwua14meyqvkc
+        foreign key (dvd_id) references public.dvd
 );
 
-create table if not exists creator
+create table if not exists public.creator
 (
-    dvd_id    varchar(255) not null
-        constraint fks0xroutb574db5dj4g7cdfiw9
-            references dvd,
-    person_id integer      not null
-        constraint fklfiq9ovibww39lq6jflx3fhx9
-            references person,
-    primary key (dvd_id, person_id)
+    dvd_id    varchar(255) not null,
+    person_id integer      not null,
+    primary key (dvd_id, person_id),
+    constraint fklfiq9ovibww39lq6jflx3fhx9
+        foreign key (person_id) references public.person,
+    constraint fks0xroutb574db5dj4g7cdfiw9
+        foreign key (dvd_id) references public.dvd
 );
 
-create table if not exists dirctor
+create table if not exists public.dirctor
 (
-    dvd_id    varchar(255) not null
-        constraint fkg3wnmxurjw9q99lxddcwmxmhp
-            references dvd,
-    person_id integer      not null
-        constraint fk2rn03idcmv63c8icldfkyq4i6
-            references person,
-    primary key (dvd_id, person_id)
+    dvd_id    varchar(255) not null,
+    person_id integer      not null,
+    primary key (dvd_id, person_id),
+    constraint fk2rn03idcmv63c8icldfkyq4i6
+        foreign key (person_id) references public.person,
+    constraint fkg3wnmxurjw9q99lxddcwmxmhp
+        foreign key (dvd_id) references public.dvd
 );
 
-create table if not exists dvd_studio
+create table if not exists public.dvd_studio
 (
-    dvd_id varchar(255) not null
-        constraint fk1krgw1lu8fcuuo7mf1whobg1u
-            references dvd,
-    studio varchar(255)
+    dvd_id varchar(255) not null,
+    studio varchar(255),
+    constraint fk1krgw1lu8fcuuo7mf1whobg1u
+        foreign key (dvd_id) references public.dvd
 );
 
-create table if not exists product_category
+create table if not exists public.product_category
 (
-    product_id  varchar(255) not null
-        constraint fk2k3smhbruedlcrvu6clued06x
-            references product,
-    category_id integer      not null
-        constraint fkkud35ls1d40wpjb5htpp14q4e
-            references category
+    product_id  varchar(255) not null,
+    category_id integer      not null,
+    constraint fkkud35ls1d40wpjb5htpp14q4e
+        foreign key (category_id) references public.category,
+    constraint fk2k3smhbruedlcrvu6clued06x
+        foreign key (product_id) references public.product
 );
 
-create table if not exists product_listmania
+create table if not exists public.product_listmania
 (
-    product_id varchar(255) not null
-        constraint fkdjct2smg0s2pc44hmdletggva
-            references product,
-    listmania  varchar(255)
+    product_id varchar(255) not null,
+    listmania  varchar(255),
+    constraint fkdjct2smg0s2pc44hmdletggva
+        foreign key (product_id) references public.product
 );
 
-create table if not exists product_similar
+create table if not exists public.product_similar
 (
-    product_id varchar(255) not null
-        constraint fkqknn5a6cl56rrvn9dvlclx9av
-            references product,
-    similar_id varchar(255) not null
-        constraint fk7f5hbigt1xka3n186q3hefghk
-            references product
+    product_id varchar(255) not null,
+    similar_id varchar(255) not null,
+    constraint fk7f5hbigt1xka3n186q3hefghk
+        foreign key (similar_id) references public.product,
+    constraint fkqknn5a6cl56rrvn9dvlclx9av
+        foreign key (product_id) references public.product
 );
 
-create table if not exists review
+create table if not exists public.review
 (
-    id         integer not null
-        primary key,
+    id         integer not null,
     content    text,
     date       date,
     helpful    integer not null,
     rating     integer not null,
     summery    varchar(255),
-    person     integer
-        constraint fkdo60vxlownqykeceb1l8uqm2v
-            references person,
-    product_id varchar(255)
-        constraint fkiyof1sindb9qiqr9o8npj8klt
-            references product
+    person     integer,
+    product_id varchar(255),
+    primary key (id),
+    constraint fkdo60vxlownqykeceb1l8uqm2v
+        foreign key (person) references public.person,
+    constraint fkiyof1sindb9qiqr9o8npj8klt
+        foreign key (product_id) references public.product
 );
 
-create table if not exists store
+create table if not exists public.store
 (
-    id     integer not null
-        primary key,
+    id     integer not null,
     name   varchar(255),
     street varchar(255),
-    zip    varchar(255)
+    zip    varchar(255),
+    primary key (id)
 );
 
-create table if not exists product_in_store
+create table if not exists public.offer
 (
-    product_id varchar(255) not null
-        constraint fk8oit7v8029dtcxlgcx8rsdwn3
-            references product,
-    store_id   integer      not null
-        constraint fkov0l5dmnkncljexe0liqgeq8
-            references store
+    id         integer          not null,
+    price      double precision not null,
+    status     varchar(255),
+    product_id varchar(255),
+    store_id   integer,
+    primary key (id),
+    constraint fk3cow2cmfxb0nrt43hxm7yu1q3
+        foreign key (product_id) references public.product,
+    constraint fk71eegvrtlg3qtybhd39ynsiqu
+        foreign key (store_id) references public.store
 );
 
-create table if not exists product_store
+create table if not exists public.product_store
 (
-    product_id varchar(255) not null
-        constraint fk5ixdrvrn9b8v6gwrws3s3brsf
-            references product,
-    store_id   integer      not null
-        constraint fk6ofo19q4q8aqicf2tifxv91et
-            references store
+    product_id varchar(255) not null,
+    store_id   integer      not null,
+    constraint fk6ofo19q4q8aqicf2tifxv91et
+        foreign key (store_id) references public.store,
+    constraint fk5ixdrvrn9b8v6gwrws3s3brsf
+        foreign key (product_id) references public.product
 );
+
+
