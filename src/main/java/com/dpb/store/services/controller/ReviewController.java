f7764198e1036413fbd1b4753102de58726ee97f
aziz <aziz.owidat@gmail.com>
2022-07-01 18:12:36 +0200

    RestController function init and finish

--- src/main/java/com/dpb/store/services/controller/ReviewController.java
+++ src/main/java/com/dpb/store/services/controller/ReviewController.java
@@ -2,8 +2,10 @@ package com.dpb.store.services.controller;
 
 import com.dpb.store.entites.Person;
 import com.dpb.store.entites.Review;
+import com.dpb.store.repos.PersonRepo;
 import com.dpb.store.repos.ReviewRepo;
 import org.hibernate.exception.ConstraintViolationException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,6 +20,8 @@ import java.util.List;
 public class ReviewController {
 
     ReviewRepo reviewRepo;
+    @Autowired
+    PersonRepo personRepo;
 
     public ReviewController(ReviewRepo reviewRepo) {
         this.reviewRepo = reviewRepo;
@@ -30,13 +34,16 @@ public class ReviewController {
             Review review1 = new Review();
             if (!review.getContent().isEmpty() && !review.getSummery().isEmpty()) {
                 if (review.getRating() > 0 && review.getRating() < 6) {
-                    review1.setPerson(review.getPerson());
+                    Person p = new Person();
+                    p.setName(review.getPerson().getName());
+                    review1.setPerson(p);
                     review1.setContent(review.getContent());
                     review1.setDate(LocalDate.now());
                     review1.setHelpful(0);
                     review1.setRating(review.getRating());
                     review1.setSummery(review.getSummery());
                     review1.setProduct_review(review.getProduct_review());
+                    personRepo.save(p);
                     reviewRepo.save(review1);
                     return review1;
                 }
